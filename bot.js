e49ca40081c719e156606d77c6f3edf1441be354
Shahriar Jaman <141053387+pabel172@users.noreply.github.com>
2023-08-29 17:58:11 +0600

    bot.js

--- bot.js
+++ bot.js
@@ -21,7 +21,7 @@ instagram : adimas_shadoet
 */
 const Telegraf = require('telegraf')
 
-const bot = new Telegraf('1675749539:AAH46KNMRschhJDMuI5dclpp9FnB0znEaK4')
+const bot = new Telegraf('6556070747:AAHsoBcwt0JDwMszMlhQTE9xeTY7tM35se0')
 const axios = require('axios')
 
 /*********[ Apikey ]*********/
